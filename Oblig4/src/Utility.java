c9468429d7848766e609ce28ba48f44bb4302928
Christopher Kragebøl Hagerup <Christopher@kragebol.no>
2015-11-04 12:31:37 +0100

    Added intToHex() with corresponding test methods.

--- Oblig4/src/Utility.java
+++ Oblig4/src/Utility.java
@@ -1,9 +1,25 @@
 
 public class Utility {
 
-	public static Object toInt(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	public static int hexToInt(String string) {
+		if(!(string.length() >= 1 && string.length() <=6)){
+			throw new IllegalArgumentException("String length should be 1-6");
+		}
+		if(string.matches("^([0-9][a-fA-F])")){
+			throw new IllegalArgumentException(
+					"String contains unsupported character");
+		}
+		int value = 0;
+		int count = 0;
+		char[] chars = string.toUpperCase().toCharArray();
+		for(int i = string.length() - 1; i >= 0; i--){
+			if((int)chars[i] <= 57 && (int)chars[i] >= 48){
+				value += (int)(chars[i]-48)*(Math.pow(16, count++)); //*(count++*16)
+			}
+			else if((int) chars[i] <= 70 && (int) chars[i] >= 65){
+				value += (int)(chars[i]-55)*(Math.pow(16, count++));
+			}
+		}
+		return value;
 	}
-
 }
