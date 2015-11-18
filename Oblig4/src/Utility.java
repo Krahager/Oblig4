d80d27d8146e3e22a942b32fad769aeb611ec5bf
Christopher Kragebøl Hagerup <Christopher@kragebol.no>
2015-11-18 16:54:25 +0100

    Moved code for testing string input to seperate method

--- Oblig4/src/Utility.java
+++ Oblig4/src/Utility.java
@@ -1,16 +1,14 @@
 import java.util.regex.Pattern;
 
 public class Utility {
-
+	private static final int BINARY = 0;
+	private static final int HEX = 1;
+	
 	public static int hexToInt(String string) {
 		if (!(string.length() >= 1 && string.length() <= 6)) {
 			throw new IllegalArgumentException("String length should be 1-6");
 		}
-		Pattern pattern = Pattern.compile("[^0-9A-Fa-f]");
-		if (pattern.matcher(string).find()) {
-			throw new IllegalArgumentException(
-					"String contains unsupported character");
-		}
+		checkString(string, HEX);
 		int value = 0;
 		int count = 0;
 		char[] chars = string.toUpperCase().toCharArray();
@@ -28,11 +26,7 @@ public class Utility {
 	public static int binToInt(String string) {
 		if (!(string.length() > 0 && string.length() <= 24))
 			throw new IllegalArgumentException("Length of string should be between " + "1 and 24");
-		
-		Pattern pattern = Pattern.compile("[^01]");
-		if (pattern.matcher(string).find())
-			throw new IllegalArgumentException("String should only contain 0 and 1");
-		
+		checkString(string, BINARY);
 		int value = 0;
 		int count = 0;
 		char[] chars = string.toCharArray();
@@ -72,9 +66,8 @@ public class Utility {
 	public static String bitwise(int operation, String bin1, String bin2) {
 		if(!(bin1.length() <= 24 && bin2.length() <= 24))
 			throw new IllegalArgumentException();
-		Pattern p = Pattern.compile("[^01]");
-		if(p.matcher(bin1).find() || p.matcher(bin2).find())
-			throw new IllegalArgumentException();
+		checkString(bin1, BINARY);
+		checkString(bin2, BINARY);
 		switch(operation){
 			case 1:
 				return bitwiseAND(bin1, bin2);
@@ -97,5 +90,16 @@ public class Utility {
 		for(int i = 0; i < bin1.length(); i++)
 			b.append((bin1.charAt(i) == '1' && bin2.charAt(i) == '1')? 1: 0);
 		return b.toString();
+	}
+	
+	private static void checkString(String s, int type){
+		Pattern p;
+		if(type == BINARY)
+			p = Pattern.compile("[^01]");
+		else
+			p = Pattern.compile("[^0-9A-Fa-f]");
+		if(p.matcher(s).find())
+			throw new IllegalArgumentException("String contains illegal characters"
+					+ "Supported pattern is " + p.toString());
 	}
 }
