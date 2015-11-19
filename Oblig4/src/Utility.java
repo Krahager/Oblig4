d34a214ef3269378c7aa5068898b126ce97bfb2f
Christopher Kragebøl Hagerup <Christopher@kragebol.no>
2015-11-19 11:54:59 +0100

    Refactored exception tests to check exception message. Moved length checks in Utility class to seperate internal method

--- Oblig4/src/Utility.java
+++ Oblig4/src/Utility.java
@@ -5,9 +5,6 @@ public class Utility {
 	private static final int HEX = 1;
 	
 	public static int hexToInt(String string) {
-		if (!(string.length() >= 1 && string.length() <= 6)) {
-			throw new IllegalArgumentException("String length should be 1-6");
-		}
 		checkString(string, HEX);
 		int value = 0;
 		int count = 0;
@@ -24,8 +21,6 @@ public class Utility {
 	}
 
 	public static int binToInt(String string) {
-		if (!(string.length() > 0 && string.length() <= 24))
-			throw new IllegalArgumentException("Length of string should be between " + "1 and 24");
 		checkString(string, BINARY);
 		int value = 0;
 		int count = 0;
@@ -64,8 +59,6 @@ public class Utility {
 	}
 
 	public static String bitwise(int operation, String bin1, String bin2) {
-		if(!(bin1.length() <= 24 && bin2.length() <= 24))
-			throw new IllegalArgumentException();
 		checkString(bin1, BINARY);
 		checkString(bin2, BINARY);
 		switch(operation){
@@ -93,13 +86,24 @@ public class Utility {
 	}
 	
 	private static void checkString(String s, int type){
+		if (s == null)
+			throw new IllegalArgumentException("String cannot be null");
+		checkLength(s, type);
 		Pattern p;
 		if(type == BINARY)
 			p = Pattern.compile("[^01]");
 		else
 			p = Pattern.compile("[^0-9A-Fa-f]");
 		if(p.matcher(s).find())
-			throw new IllegalArgumentException("String contains illegal characters"
-					+ "Supported pattern is " + p.toString());
+			throw new IllegalArgumentException("String contains illegal characters. Supported pattern is " + p.toString());
+	}
+	
+	private static void checkLength(String s, int type){
+		String a = (type == BINARY)? "Binary": "Hexadecimal";
+		int length = (type == BINARY)? 24: 6;
+		if (s.length() == 0)
+			throw new IllegalArgumentException("Length of string cannot be 0");
+		if (s.length() > length)
+			throw new IllegalArgumentException(a + " string too long. Maximum supported length is " + length + ". Length was " + s.length());
 	}
 }
--- Oblig4/test/UtilityTest.java
+++ Oblig4/test/UtilityTest.java
@@ -1,42 +1,59 @@
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 public class UtilityTest {
 	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
 	
 	@Before
 	public void setUp() throws Exception {
 	}
 
-	@Test(expected = IllegalArgumentException.class)
+	@Test
 	public void intToHex_WhenGivenNumberLessThan0_ShouldThrowIllegalArgumentException(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("The number can't be less than 0");
 		Utility.intToHex(-32);
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test
 	public void hexToInt_WhenGivenSevenDigitHex_ShouldThrowIllegalArgumentException(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Hexadecimal string too long. Maximum supported length is 6. Length was 7");
 		Utility.hexToInt("1000000");
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test
 	public void hexToInt_WhenGivenIllegalCharacters_ShouldThrowIllegalArgumentException(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("String contains illegal characters. Supported pattern is [^0-9A-Fa-f]");
 		Utility.hexToInt("h1k");
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test
 	public void binToInt_WhenGivenMoreThan24Digits_ShouldThrowIllegalArgumentException(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Binary string too long. Maximum supported length is 24. Length was 25");
 		Utility.binToInt("1111111111111111111111111");
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test
 	public void binToInt_WhenGivenIllegalCharacters_ShouldThrowIllegalArgumentException(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("String contains illegal characters. Supported pattern is [^01]");
 		Utility.binToInt("910");
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
+	@Test
 	public void intToBin_WhenGivenValueOver16777215_ShouldThrowIllegalArgumentException(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Number higher than 24 bits can support");
 		Utility.intToBin(167772155);
 	}
 	
@@ -50,5 +67,12 @@ public class UtilityTest {
 	public void bitwise_WhenGiven1_ShouldPerformBitwiseAND(){
 		assertThat(Utility.bitwise(1, "110101000100101010101011", "100101000100101010101111"),
 				is("100101000100101010101011"));
+	}
+	
+	@Test
+	public void bitwise_WhenGivenStringLongerThan24_ShouldThrowIllegalArgumentException(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Binary string too long. Maximum supported length is 24. Length was 47");
+		Utility.bitwise(1, "00000000000000000000000000000000000000000000000", "101010101010101010101010101010");
 	}
 }
